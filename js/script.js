ecc1a0f842b41bc256909296880e140859a10cc0
kiara-v <michelle_vong@brown.edu>
2024-02-24 02:32:38 -0500

    change slider and organize files

---
+++ js/script.js
@@ -0,0 +1,96 @@
+const IsMobile = () => {
+    let width =
+        window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.getElementsByTagName("body")[0].clientWidth;
+
+    if (width <= 736) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const ResizeHeader = () => {
+    let width =
+        window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.getElementsByTagName("body")[0].clientWidth;
+
+    if (width <= 815) {
+        if (document.getElementsByClassName("hamburger").length <= 0) {
+            let header = document.getElementsByClassName("header")[0];
+            let main_nav = header.getElementsByClassName("main-nav")[0];
+            let right_nav = header.getElementsByClassName("right-nav")[0];
+
+            let hamburger = document.createElement("div");
+            hamburger.classList.add("hamburger");
+            hamburger.innerHTML = `<svg fill="currentColor" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+</svg>`;
+            main_nav.remove();
+            right_nav.remove();
+
+            let bottom_header = document.createElement("div");
+            bottom_header.classList.add("header-bottom");
+            bottom_header.append(main_nav, right_nav);
+            bottom_header.style.display = "none";
+
+            hamburger.addEventListener("click", function () {
+                if (bottom_header.style.display == "none") {
+                    bottom_header.style.display = "flex";
+                    header.style.paddingBottom = "9px";
+                    header.style.paddingTop = "9px";
+                } else {
+                    bottom_header.style.display = "none";
+                    header.style.paddingBottom = "0px";
+                    header.style.paddingTop = "0px";
+                }
+            });
+
+            header.classList.add("header-change");
+
+            let top_header = document.createElement("div");
+            top_header.classList.add("header-top");
+            top_header.append(header.children[0], hamburger);
+
+            header.innerHTML = "";
+            header.append(top_header, bottom_header);
+        }
+    } else {
+        if (document.getElementsByClassName("hamburger").length > 0) {
+            let header = document.getElementsByClassName("header")[0];
+            let main_nav = header.getElementsByClassName("main-nav")[0];
+            let right_nav = header.getElementsByClassName("right-nav")[0];
+            let brand = header.getElementsByClassName("brand")[0];
+
+            header.classList.remove("header-change");
+            header.children[0].remove();
+            header.children[0].remove();
+            header.append(brand, main_nav, right_nav);
+        }
+    }
+};
+
+const ScrollFunction = () => {
+    let header = document.getElementsByClassName("header")[0];
+    if (document.documentElement.scrollTop > 1) {
+        header.classList.add("header-active");
+    } else {
+        header.classList.remove("header-active");
+    }
+};
+
+const MakeJumbotron = () => {
+    let height =
+        window.innerHeight ||
+        document.documentElement.clientHeight ||
+        document.getElementsByTagName("body")[0].clientHeight;
+
+    document.getElementsByClassName("top")[0].style.height = `${height}px`;
+};
+
+const SmoothScroll = (id) => {
+    let element = document.getElementById(id);
+    element.scrollIntoView({ behavior: "smooth", block: "center" });
+};
